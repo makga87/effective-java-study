ffe67ac1793a06242b69e1552256f9c253922e70
jeonghun3.kim <jeonghun3.kim@bugs.co.kr>
2023-01-14 12:06:34 +0900

    [ADD] 스터디 룰 공유 및 기본 프로젝트 구조 셋팅

---
+++ src/main/java/com/study/effectivejava/item1/FlyweightPattern.java
@@ -0,0 +1,8 @@
+package com.study.effectivejava.item1;
+
+public class FlyweightPattern {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/study/effectivejava/item1/FlyweightPatternTest.java
@@ -0,0 +1,12 @@
+package com.study.effectivejava.item1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlyweightPatternTest {
+    @Test
+    void test() {
+
+    }
+}
